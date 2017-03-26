7322018adca49661a9086a527d13cbc143c2f7f7
Alberto Rodríguez <albrodriguezr@unal.edu.co>
2017-03-25 22:13:06 -0500

    Se actualiza el scrip con la totalidad de verbos de la librería dplyr

--- Manipulacion.R
+++ Manipulacion.R
@@ -13,3 +13,18 @@ iris2 <- mutate(iris1, Nueva = Sepal.Length / 10)
 # Verbo: filter
 
 iris3 <- filter(iris2, Species %in% c("setosa", "virginica"))
+
+# Verbo: arrange
+
+iris4 <- arrange(iris3, Nueva)
+iris5 <- arrange(iris3, desc(Nueva))
+
+# Verbo: summarise
+
+iris6 <- summarise(iris5, Media1 = mean(Sepal.Length), Media2 = mean(Nueva))
+
+# Verbo: summarise + group_by
+
+iris7 <- group_by(iris5, Species)
+iris8 <- summarise(iris7, Media1 = mean(Sepal.Length), Media2 = mean(Nueva))
+
